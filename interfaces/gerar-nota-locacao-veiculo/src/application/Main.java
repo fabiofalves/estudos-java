36c1d3394bdbdcc080872a51b52535c11d5eb89e
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-07-16 18:00:33 -0300

    Exemplo sem interface

--- interfaces/gerar-nota-locacao-veiculo/src/application/Main.java
+++ interfaces/gerar-nota-locacao-veiculo/src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 
 import entities.CarRental;
 import entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -9,7 +11,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] agrs){
+    public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
@@ -27,7 +29,19 @@ public class Main {
         LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
         CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
-        
+
+        System.out.print("Enter price per hour: ");
+        Double pricePerHour = sc.nextDouble();
+
+        System.out.print("Enter price per day: ");
+        Double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        System.out.println("INVOICE:");
+        System.out.println("Basic payment: " + String.format("%.2f", carRental.getInvoice().getBasicPayment()));
+        System.out.println("Tax: " + String.format("%.2f", carRental.getInvoice().getTax()));
+        System.out.println("Total payment: " + String.format("%.2f", carRental.getInvoice().getTotalPayment()));
 
         sc.close();
     }
