1778b6303c4fe945f1e727a5faa8088e518661ff
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-07-16 14:32:35 -0300

    adicionado logica para ler e criar arquivo no Main

--- trabalhando-com-arquivos/ler-arquivo-csv/src/application/Main.java
+++ trabalhando-com-arquivos/ler-arquivo-csv/src/application/Main.java
@@ -1,5 +1,15 @@
 package application;
 
+import entities.Product;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,7 +17,64 @@ public class Main {
     public static void main(String[] args){
 
         Locale.setDefault(Locale.US);
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
+        List<Product> list = new ArrayList<>();
+
+        //Captura do usuário o diretório da pasta
+        System.out.print("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
+
+        //cria um objeto do tipo File que aponta para a origem do arquivo
+        File sourceFile = new File(sourceFileStr);
+
+        //Captura o endereço da pasta de origem
+        String sourceFolderStr = sourceFile.getParent();
+
+        //Cria uma subpasta chamada out dentro da pasta de origem.
+        //e retorna verdadeiro se foi criado com sucesso.
+        boolean success = new File(sourceFolderStr + "\\out").mkdir();
+
+        System.out.println("Folder created: " + success);
+
+        //cria o arquivo de destino summary.csv na subpasta out dentro da pasta de origem
+        String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))){
+
+            String itemCsv =  br.readLine();//a variável itemCsv recebe cada linha do arquivo
+
+            //Enquanto a linha não for vazia(null)
+            while(itemCsv != null){
+
+                String[] fields = itemCsv.split(",");//divide cada valor separado por ","
+
+                String name = fields[0];
+                Double price = Double.parseDouble(fields[1]);//Parse converte string para o tipo da variável
+                Integer quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                itemCsv = br.readLine();
+            }
+
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+
+                for(Product item : list){
+                    bw.write(item.toString());
+                    bw.newLine();
+                }
+
+                System.out.println("Summary file created successfully!");
+
+            } catch(IOException e){
+                System.out.print("Error writing file: " + e.getMessage());
+            }
+
+        }catch(IOException e){
+            System.out.println("Error reading file: " + e.getMessage());
+        }catch (Exception e){
+            System.out.println("Parse error: " + e.getMessage());
+        }
     }
 }
