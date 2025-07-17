5a219f2c38e2df4ae448ce896c4ba1d8860235f2
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-07-17 14:11:05 -0300

    Criado classes, serviços e programa principal

---
+++ interfaces/gerar-parcelas-contrato/src/model/services/ContractService.java
@@ -0,0 +1,14 @@
+package model.services;
+
+import model.entities.Contract;
+
+public class ContractService {
+
+    private OnlinePaymentService onlinePaymentService;
+
+    public void processContract (Contract contract, Integer month){
+
+
+    }
+
+}
---
+++ interfaces/gerar-parcelas-contrato/src/model/services/PaypalService.java
@@ -0,0 +1,13 @@
+package model.services;
+
+public class PaypalService implements OnlinePaymentService{
+
+    public Double paymentFee (Double amount){
+
+    }
+
+    public Double interest (Double amount, Integer month){
+
+    }
+
+}
