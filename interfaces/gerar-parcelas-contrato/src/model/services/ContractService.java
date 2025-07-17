830a4e705f3ecc8f6bfe21586d55b181d83d11e0
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-07-17 16:36:15 -0300

    corrigido logica de calcular taxa de pagamento no service ContractService

--- interfaces/gerar-parcelas-contrato/src/model/services/ContractService.java
+++ interfaces/gerar-parcelas-contrato/src/model/services/ContractService.java
@@ -12,16 +12,14 @@ public class ContractService {
     public void processContract (Contract contract, Integer month){
 
         LocalDate dueDate;
-        Double amount = contract.getTotalValue() / month;
         Integer[] numberInstallment = new Integer[month];
 
         for(int i = 0; i < numberInstallment.length; i++){
             numberInstallment[i] = i+1;
-
+            Double amount = contract.getTotalValue() / month;
             Double taxMonth = onlinePaymentService.interest(amount, numberInstallment[i]);
             amount += taxMonth;
             Double paymentFee = amount + onlinePaymentService.paymentFee(amount);
-
             dueDate = contract.getDate().plusMonths(numberInstallment[i]);
 
             contract.addInstallment(new Installment(dueDate, paymentFee));
